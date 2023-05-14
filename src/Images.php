6eb928001aff20f7bc74112b6ff5b8288ec96f30
Your Name <you@example.com>
2023-05-14 21:01:47 +0300

    + COLOR DETERM LOGGING

--- src/Images.php
+++ src/Images.php
@@ -4,16 +4,13 @@ namespace ImagesBundle;
 
 use League\ColorExtractor\Color;
 use League\ColorExtractor\Palette;
-
 use ImagesBundle\Api\Adapter\TheColorAdapter;
 use ImagesBundle\Api\Adapter\ColorPizzaAdapter;
 use ImagesBundle\Api\ImagesApi;
 use ImagesBundle\Api\ApiLoader;
-
 use ourcodeworld\NameThatColor\ColorInterpreter;
-
 use App\Storage\RequestHeadersStorage;
-
+use Psr\Log\LoggerInterface;
 use FilesBundle\Image;
 
 class Images {
@@ -24,10 +21,11 @@ class Images {
         protected ImagesApi $imagesApi,
         protected ApiLoader $apiLoader,
         protected RequestHeadersStorage $requestHeadersStorage,
+        protected LoggerInterface $logger,
     ) {
         $this->apiLoader->setApiAdapters(
-            $colorPizzaAdapter,
             $theColorAdapter,
+            $colorPizzaAdapter,
             $imagesApi,
         );
     }
@@ -37,11 +35,15 @@ class Images {
 
         $topColors = $this->getMostUsedColors($img, 10);
 
+        $topColorsHex = [];
+
         $colorsFrequency = [];
 
         foreach($topColors as $color => $count) {
 
             $colorHex = Color::fromIntToHex($color);
+
+            $topColorsHex[ $colorHex ] = $count;
 
             $colorName = $this->getColorName($colorHex);
 
@@ -63,10 +65,18 @@ class Images {
                 $colorsFrequency[ $colorNameFound ]++;
             }
         }
+
+        $this->logger->debug(json_encode([
+            "image" => $img->getImageSignature(),
+            "topColors " => $topColorsHex,
+            "matchedColors" => $colorsFrequency,
+        ], JSON_UNESCAPED_SLASHES));
+
         if (count($colorsFrequency)) {
             arsort($colorsFrequency);
             return array_key_last($colorsFrequency);  
         }
+
         return null;
     }
 
